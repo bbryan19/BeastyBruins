054dd9bd44bc3e95a907a1983a65872e9e5116d9
Blake Bryan <bbryan19@georgefox.edu>
2022-11-12 12:01:00 -0800

    skeleton

--- bruinbytes/src/App.js
+++ bruinbytes/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="bruin.png" alt="cyborg bear"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Play with <a>Logic Gates</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
